43b407dfaac134a2ff3dd542bafdaa4ead8a2a42
Aleksandar Petrov <aleksanndar1995@gmail.com>
2019-07-23 08:06:13 +0200

    arg fix

--- batch_Lambert_sampling.py
+++ batch_Lambert_sampling.py
@@ -12,8 +12,8 @@ parser.add_argument('--njobs', help='number of threads')
 parser.add_argument('--objects', help='number of objects')
 args = parser.parse_args()
 
-dt = args['dt'] # descretization step, in days
-output_prefix = args['output']+'/'
+dt = args.dt # descretization step, in days
+output_prefix = args.output+'/'
 
 # Make the tmp folder if doesn't exist
 if not os.path.exists(output_prefix):
@@ -38,12 +38,12 @@ def full_process(obj_idx_tuple):
     np.save(output_prefix+str(id_start).zfill(3)+'_'+str(id_end).zfill(3), DV_matrix)
 
 # gtoc2gr2_objects = np.arange(96,271+1)
-gtoc2gr2_objects = np.arange(96,96+args['dt'])
+gtoc2gr2_objects = np.arange(96,96+args.objects)
 all_pairs = list()
 for i in gtoc2gr2_objects:
     for j in gtoc2gr2_objects:
         if i != j:
             all_pairs.append((i,j))
 
-p = ProcessingPool(args['njobs'])
+p = ProcessingPool(args.njobs)
 p.map(full_process, all_pairs)
