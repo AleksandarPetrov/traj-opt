97c815b0acfe0c825a58f2b741a1a2c3b894746a
Aleksandar Petrov <aleksanndar1995@gmail.com>
2019-08-06 22:54:56 +0200

    Docker updates + README

---
+++ README.md
@@ -0,0 +1,42 @@
+# Optimizing multi-rendezvous spacecraft trajectories: ∆V matrices and sequence selection
+
+Code fir the experiments.
+
+## Installation
+
+This code uses ephemeris data and Lambert targeting from [pyKEP](https://github.com/esa/pykep) so pyKEP needs to be installed. Also required are: [pygmo](https://github.com/esa/pagmo2) [pathos](https://github.com/uqfoundation/pathos) and [pyYAML](https://github.com/yaml/pyyaml):
+
+```
+pip3 install pygmo pathos pyYAML pykep
+```
+
+A Docker container with everything neccessary installed is also provided at `aleksandarpetrov/traj-opt`.
+
+
+## Getting Started
+
+Running an experiment consists of:
+1. Generating ∆V matrices with the `batch_Lambert_sampling.py` script, e.g:
+```
+python3 /files/batch_Lambert_sampling.py --dt 80 --njobs 8 --output data/DV_20obj_dt80_raw --objects 20
+```
+
+2. Applying wait-adjustment to the generated matrices with the `wait_adjusting.py` script, e.g:
+```
+python3 /files/wait_adjusting.py --njobs 8 --input data/DV_20obj_dt80_raw --output data/DV_20obj_dt80_wa
+```
+
+3. Computing sequences of 5 objects via direct concatenation with the `main.py` script:
+```
+python3 /files/main.py --njobs 8 --input data/DV_20obj_dt80_wa --output data/DV_20obj_dt80_dced --dvtol 10000 --objects 20 --results data/20obj_dt80_results.yml --inmemory true
+```
+
+All this can be run in the provided Docker container by using the provided `run_all.sh` as a template. Note that you'd need to have Docker installed to be able to use it.
+
+## Authors
+
+* Aleksandar Petrov
+
+## License
+
+Contact the authors
---
+++ run_all.sh
@@ -0,0 +1,35 @@
+echo "SAMPLING THE DV MATRICES FOR DT 80"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/batch_Lambert_sampling.py --dt 80 --njobs 16 --output /data/DV_20obj_dt80_raw --objects 20
+
+echo "SAMPLING THE DV MATRICES FOR DT 40"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/batch_Lambert_sampling.py --dt 40 --njobs 16 --output /data/DV_20obj_dt40_raw --objects 20
+
+echo "SAMPLING THE DV MATRICES FOR DT 20"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/batch_Lambert_sampling.py --dt 20 --njobs 16 --output /data/DV_20obj_dt20_raw --objects 20
+
+echo "SAMPLING THE DV MATRICES FOR DT 10"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/batch_Lambert_sampling.py --dt 10 --njobs 16 --output /data/DV_20obj_dt10_raw --objects 20
+
+echo "APPLYING WAIT ADJUSTMENT FOR DT 80"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/wait_adjusting.py --njobs 16 --input /data/DV_20obj_dt80_raw --output /data/DV_20obj_dt80_wa
+
+echo "APPLYING WAIT ADJUSTMENT FOR DT 40"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/wait_adjusting.py --njobs 16 --input /data/DV_20obj_dt40_raw --output /data/DV_20obj_dt40_wa
+
+echo "APPLYING WAIT ADJUSTMENT FOR DT 20"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/wait_adjusting.py --njobs 16 --input /data/DV_20obj_dt20_raw --output /data/DV_20obj_dt20_wa
+
+echo "APPLYING WAIT ADJUSTMENT FOR DT 10"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/wait_adjusting.py --njobs 16 --input /data/DV_20obj_dt10_raw --output /data/DV_20obj_dt10_wa
+
+echo "STARTING DIRECT CONCATENATION FOR DT 80"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/main.py --njobs 16 --input /data/DV_20obj_dt80_wa --output /data/DV_20obj_dt80_dced --dvtol 10000 --objects 20 --results /data/20obj_dt80_results.yml --inmemory true
+
+echo "STARTING DIRECT CONCATENATION FOR DT 40"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/main.py --njobs 16 --input /data/DV_20obj_dt40_wa --output /data/DV_20obj_dt40_dced --dvtol 10000 --objects 20 --results /data/20obj_dt40_results.yml --inmemory true
+
+echo "STARTING DIRECT CONCATENATION FOR DT 20"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/main.py --njobs 16 --input /data/DV_20obj_dt20_wa --output /data/DV_20obj_dt20_dced --dvtol 10000 --objects 20 --results /data/20obj_dt20_results.yml --inmemory true
+
+echo "STARTING DIRECT CONCATENATION FOR DT 10"
+docker run -it -v "$(pwd)":/data -m=20g --cpus=16 --rm aleksandarpetrov/traj-opt python3.7 /files/main.py --njobs 12 --input /data/DV_20obj_dt10_wa --output /data/DV_20obj_dt10_dced --dvtol 10000 --objects 20 --results /data/20obj_dt10_results.yml --inmemory true
